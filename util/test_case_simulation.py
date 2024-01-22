b093025522b56e305176a0c584c44099d5217db6
zhe.zhu <zhe.zhu@samsung.com>
2024-01-22 14:14:44 -0800

    1st round of test case generation finished

--- util/test_case_simulation.py
+++ util/test_case_simulation.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 from random import randint
 
@@ -62,6 +63,7 @@ def quadsect(rect, factor):
             rect_bl,
             rect_br]
 
+
 def square_subregion(rect):
     """ Return a square rectangle centered within the given rectangle """
     w, h = rect.width, rect.height  # cache properties
@@ -79,7 +81,7 @@ def visualize():
     random.seed()
 
     NUM_RECTS = 200
-    REGION = Rect(0, 0, 2000, 2000)
+    REGION = Rect(0, 0, 1280, 1280)
     # call quadsect() until at least the number of rects wanted has been generated
     rects = [REGION]   # seed output list
     while len(rects) <= NUM_RECTS:
@@ -112,21 +114,85 @@ def visualize():
         draw.rectangle([(rect.min.x, rect.min.y), (rect.max.x, rect.max.y)],
                    fill=fill, outline=outline)
 
-    # first draw outlines of all the non-overlapping rectanges generated
-    for rect in rects:
-        draw_rect(rect, outline=LIGHT_GRAY)
 
     # then draw the random sample of them selected
     for rect in sample:
         draw_rect(rect, fill=RECT_COLOR, outline=WHITE)
 
-    # and lastly convert those into squares and re-draw them in another color
-    for rect in sample:
-        draw_rect(square_subregion(rect), fill=SQUARE_COLOR, outline=WHITE)
-
     filename = 'square_quadsections.png'
     image.save(filename, "PNG")
     print(repr(filename), 'output image saved')
+
+
+def generate_rects(sample_num, rect_num, img_h, img_w, out_dir):
+    """
+    Generate randon rects and save the results
+    """
+    random.seed()
+    NUM_RECTS = rect_num
+    REGION = Rect(0, 0, img_w, img_h)
+
+    # Prepare output dir
+    out_rect_dir = os.path.join(out_dir, "rect")
+    if not os.path.exists(out_rect_dir):
+        os.makedirs(out_rect_dir)
+    out_vis_dir = os.path.join(out_dir, "vis")
+    if not os.path.exists(out_vis_dir):
+        os.makedirs(out_vis_dir)
+
+    # Generate sample_num samples
+    for i in range(sample_num):
+        out_sample_id = f"{rect_num}_{i}"
+        out_txt_name = out_sample_id + ".txt"
+        out_txt_path = os.path.join(out_rect_dir, out_txt_name)
+
+        out_img_name = out_sample_id + ".png"
+        out_img_path = os.path.join(out_vis_dir, out_img_name)
+
+        # call quadsect() until at least the number of rects wanted has been generated
+        rects = [REGION]  # seed output list
+        while len(rects) <= NUM_RECTS:
+            rects = [subrect for rect in rects
+                    for subrect in quadsect(rect, 10)]
+
+        random.shuffle(rects)  # mix them up
+        sample = random.sample(rects, NUM_RECTS)  # select the desired number
+        print('%d out of the %d rectangles selected' % (NUM_RECTS, len(rects)))
+
+        # Save to txt
+        with open(out_txt_path, 'w') as writer:
+            for j in range(rect_num):
+                x1, y1, x2, y2 = rects[j].min.x, rects[j].min.y, rects[j].max.x, rects[j].max.y
+                writer.write(f"{x1} {y1} {x2} {y2}\n")
+
+        #################################################
+        # extra credit - create an image file showing results
+        from PIL import Image, ImageDraw
+        def gray(v):
+            return tuple(int(v * 255) for _ in range(3))
+
+        BLACK, DARK_GRAY, GRAY = gray(0), gray(.25), gray(.5)
+        LIGHT_GRAY, WHITE = gray(.75), gray(1)
+        RED, GREEN, BLUE = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+        CYAN, MAGENTA, YELLOW = (0, 255, 255), (255, 0, 255), (255, 255, 0)
+        BACKGR, SQUARE_COLOR, RECT_COLOR = (245, 245, 87), (255, 73, 73), (37, 182, 249)
+
+        imgx, imgy = REGION.max.x + 1, REGION.max.y + 1
+        image = Image.new("RGB", (imgx, imgy), BACKGR)  # create color image
+        draw = ImageDraw.Draw(image)
+
+        def draw_rect(rect, fill=None, outline=WHITE):
+            draw.rectangle([(rect.min.x, rect.min.y), (rect.max.x, rect.max.y)],
+                        fill=fill, outline=outline)
+
+
+
+        # draw the random sample of them selected
+        for rect in sample:
+            draw_rect(rect, fill=RECT_COLOR, outline=WHITE)
+
+        image.save(out_img_path, "PNG")
+        print(repr(out_img_path), 'output image saved')
 
 
 def task_test_ori_alg():
@@ -136,8 +202,35 @@ def task_test_ori_alg():
     visualize()
 
 
+def task_generate_exp_data():
+    """
+    Generate the test samples to use in the experiment
+    """
+    out_dir_p = "D:\\tmp\\bbh\\test_case"
+    sample_num = 100
+    rect_num_list = [10, 100, 1000, 10000, 100000, 1000000]
+    img_size_list = [[256, 256],
+                     [512, 512],
+                     [1024, 1024],
+                     [2048, 2048],
+                     [8192, 8192],
+                     [32768, 32768]]
+    for i in range(len(rect_num_list)):
+        rect_num = rect_num_list[i]
+        h, w = img_size_list[i][0], img_size_list[i][1]
+        out_dir = os.path.join(out_dir_p, f"{rect_num}")
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        generate_rects(sample_num=sample_num,
+                       rect_num=rect_num,
+                       img_h=h,
+                       img_w=w,
+                       out_dir=out_dir)
+
+
 def main():
-    task_test_ori_alg()
+    #task_test_ori_alg()
+    task_generate_exp_data()
 
 
 if __name__ == "__main__":
