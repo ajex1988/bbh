3d28678859fda1d04b7df87d09395753de319739
zhe.zhu <zhe.zhu@samsung.com>
2024-01-22 11:48:55 -0800

    Modify the original alg so we can simulate non-overlapping rects

--- util/test_case_simulation.py
+++ util/test_case_simulation.py
@@ -5,6 +5,7 @@ from random import randint
 https://stackoverflow.com/questions/4373741/how-can-i-randomly-place-several-non-colliding-rects
 """
 
+
 class Point(object):
     def __init__(self, x, y):
         self.x, self.y = x, y
@@ -12,6 +13,7 @@ class Point(object):
     @staticmethod
     def from_point(other):
         return Point(other.x, other.y)
+
 
 class Rect(object):
     def __init__(self, x1, y1, x2, y2):
@@ -23,10 +25,11 @@ class Rect(object):
     def from_points(p1, p2):
         return Rect(p1.x, p1.y, p2.x, p2.y)
 
-    width  = property(lambda self: self.max.x - self.min.x)
+    width = property(lambda self: self.max.x - self.min.x)
     height = property(lambda self: self.max.y - self.min.y)
 
 plus_or_minus = lambda v: v * [-1, 1][(randint(0, 100) % 2)]  # equal chance +/-1
+
 
 def quadsect(rect, factor):
     """ Subdivide given rectangle into four non-overlapping rectangles.
@@ -36,15 +39,28 @@ def quadsect(rect, factor):
     # pick a point in the interior of given rectangle
     w, h = rect.width, rect.height  # cache properties
     center = Point(rect.min.x + (w // 2), rect.min.y + (h // 2))
-    delta_x = plus_or_minus(randint(0, w // factor))
-    delta_y = plus_or_minus(randint(0, h // factor))
-    interior = Point(center.x + delta_x, center.y + delta_y)
+    rect_tl = Rect(center.x - randint(0, w // factor),
+                   center.y - randint(0, h // factor),
+                   rect.min.x + randint(0, w // factor),
+                   rect.min.y + randint(0, h // factor))
+    rect_tr = Rect(center.x + randint(0, w // factor),
+                   center.y - randint(0, h // factor),
+                   rect.max.x - randint(0, w // factor),
+                   rect.min.y + randint(0, h // factor))
+    rect_bl = Rect(center.x - randint(0, w // factor),
+                   center.y + randint(0, h // factor),
+                   rect.min.x + randint(0, w // factor),
+                   rect.max.y - randint(0, h // factor))
+    rect_br = Rect(center.x + randint(0, w // factor),
+                   center.y + randint(0, h // factor),
+                   rect.max.x - randint(0, w // factor),
+                   rect.max.y - randint(0, h // factor))
 
     # create rectangles from the interior point and the corners of the outer one
-    return [Rect(interior.x, interior.y, rect.min.x, rect.min.y),
-            Rect(interior.x, interior.y, rect.max.x, rect.min.y),
-            Rect(interior.x, interior.y, rect.max.x, rect.max.y),
-            Rect(interior.x, interior.y, rect.min.x, rect.max.y)]
+    return [rect_tl,
+            rect_tr,
+            rect_bl,
+            rect_br]
 
 def square_subregion(rect):
     """ Return a square rectangle centered within the given rectangle """
@@ -62,13 +78,13 @@ def square_subregion(rect):
 def visualize():
     random.seed()
 
-    NUM_RECTS = 20
-    REGION = Rect(0, 0, 640, 480)
+    NUM_RECTS = 200
+    REGION = Rect(0, 0, 2000, 2000)
     # call quadsect() until at least the number of rects wanted has been generated
     rects = [REGION]   # seed output list
     while len(rects) <= NUM_RECTS:
         rects = [subrect for rect in rects
-                        for subrect in quadsect(rect, 3)]
+                        for subrect in quadsect(rect, 10)]
 
     random.shuffle(rects)  # mix them up
     sample = random.sample(rects, NUM_RECTS)  # select the desired number
@@ -111,3 +127,18 @@ def visualize():
     filename = 'square_quadsections.png'
     image.save(filename, "PNG")
     print(repr(filename), 'output image saved')
+
+
+def task_test_ori_alg():
+    """
+    Test the original algorithm pulled from github
+    """
+    visualize()
+
+
+def main():
+    task_test_ori_alg()
+
+
+if __name__ == "__main__":
+    main()
