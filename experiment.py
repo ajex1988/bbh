df85c7a3bd3d75f14353a0af15bddcef28363866
zhe.zhu <zhe.zhu@samsung.com>
2024-01-22 16:58:18 -0800

    running time test code finished but not tested

--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 from util.extract_bbox_from_real_cases import load_bbox_from_txt
 import logging
@@ -29,6 +30,44 @@ def exp_homepage_case_test():
     t_fast_end = time.perf_counter()
     logging.info(f"Fast approach: {t_fast_end-t_fast_start}")
 
+
+def running_time_test_by_rect_num(in_dir, out_path, alg_name='FAST'):
+    """
+    Calculate the running time of a particular rect_num
+    This will run sample_num time and calculate the average
+    Algorithm name can be either BF or FAST
+    """
+    logging.basicConfig(filename=out_path, encoding='utf-8', level=logging.INFO)
+    sample_file_list = os.listdir(in_dir)
+    t_list = []
+    for sample_file_name in sample_file_list:
+        sample_file_path = os.path.join(in_dir, sample_file_name)
+        bbox_list = load_bbox_from_txt(txt_file_path=sample_file_path)
+
+        if alg_name == "FAST":
+            alg = BBHFast(bboxes=bbox_list)
+        elif alg_name == "BF":
+            alg = BBHNaive(bboxes=bbox_list)
+        else:
+            raise Exception("Unknown Algorithm")
+        t_start = time.perf_counter()
+        _ = alg.merge()
+        t_end = time.perf_counter()
+        t_cost = t_end - t_start
+        t_list.append(t_cost)
+
+        logging.info(f'Sample {sample_file_name} cost: {t_cost}')
+    t_avg = sum(t_list) / len(t_list)
+    logging.info(f"Avg: {t_avg}")
+
+
+def task_running_time_exp():
+    in_dir = sys.argv[1]
+    out_path = sys.argv[2]
+    alg_name = sys.argv[3]
+    running_time_test_by_rect_num(in_dir=in_dir,
+                                  out_path=out_path,
+                                  alg_name=alg_name)
 
 def main():
     exp_homepage_case_test()
