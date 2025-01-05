bd567990b9d782ebcc403b775264addcb70c1684
zhe.zhu <zhe.zhu@samsung.com>
2025-01-05 15:33:52 -0800

    save all changes so that to use vsc

---
+++ experiment_realworld_multilabels.py
@@ -0,0 +1,78 @@
+import os
+import json
+from PIL import Image, ImageDraw
+from bbh.bbh import BBHFastMultiLabel
+
+def name2label_dict():
+    dict = {}
+    dict["ignore"] = 0
+    dict["pedestrian"] = 1
+    dict["rider"] = 2
+    dict["sitting person"] = 3
+    dict["person (other)"] = 4
+    dict["person group"] = 5
+    return dict
+
+def label2color_dict():
+    dict = {}
+    dict[0] = (255, 255, 0)
+    dict[1] = (255, 0, 0)
+    dict[2] = (0, 255, 0)
+    dict[3] = (0, 0, 255)
+    dict[4] = (0, 255, 255)
+    dict[5] = (255, 0, 255)
+    return dict
+
+def task_visualize_cityperson_multilabel():
+    ann_file_path = r"D:\Data\BBH_Exp\CityPersons\multi_labels\annotation_filtered.json"
+    img_dir = "D:\\Data\\BBH_Exp\\CityPersons\\city_persons\\images"
+    out_dir = "D:\\Data\\BBH_Exp\\CityPersons\\multi_labels\\results"
+
+    select_list = [-20, -10, -5]
+
+    name2label = name2label_dict()
+    label2color = label2color_dict()
+
+    with open(ann_file_path, 'r') as f:
+        info = json.load(f)
+
+    for fid in info:
+        city_name, no_1, no_2, gtbox_name = info[fid]
+        img_name = f"{city_name}_{city_name}_{no_1}_{no_2}.png"
+        img_path = os.path.join(img_dir, img_name)
+        img = Image.open(img_path)
+        img = img.convert('RGB')
+
+        bboxes = []
+        labels = []
+
+        labeled_bboxes = info[fid]
+        for lname in labeled_bboxes:
+            bbox = labeled_bboxes[lname]
+            bboxes.extend(bbox)
+            labels.append([name2label[lname]]*len(bbox))
+
+        alg = BBHFastMultiLabel(bboxes=bboxes, labels=labels)
+        bbh, bbh_label = alg.merge()
+
+        for i in select_list:
+            bbox_list = bbh[i]
+            bbox_label_list = bbh_label[i]
+
+            image = img.copy()
+            draw = ImageDraw.Draw(image)
+            for j, bbox in enumerate(bbox_list):
+                color = label2color[bbox_label_list[j]]
+                draw.rectangle([(bbox[0], bbox[1]), (bbox[2], bbox[3])],
+                               fill=None, outline=color)
+            image.save(os.path.join(out_dir, f"{img_name[:-4]}_{abs(i)}.png"))
+
+
+
+
+def main():
+    task_visualize_cityperson_multilabel()
+
+
+if __name__ == '__main__':
+    main()
--- util/cityperson_multi_label_preprocess.py
+++ util/cityperson_multi_label_preprocess.py
@@ -1,7 +1,9 @@
 import os
+import json
+import tqdm
 
 
-def parse_cityperson_annotation(ann_dir):
+def parse_cityperson_annotation(ann_dir, total_obj_num_thre=50):
     """
     Parse the annotation files of the cityperson dataset.
     For this dataset, each image has a corresponding annotation file, and images are organized by cities. For example:
@@ -23,22 +25,70 @@ def parse_cityperson_annotation(ann_dir):
     """
     city_name_list = os.listdir(ann_dir)
     info = {}
-    for city_name in city_name_list:
+    for city_name in tqdm.tqdm(city_name_list):
         city_dir = os.path.join(ann_dir, city_name)
         ann_file_list = os.listdir(city_dir)
+        for ann_file_name in ann_file_list:
+            ann_file_path = os.path.join(city_dir, ann_file_name)
+            with open(ann_file_path, 'r') as f:
+                ann_data = json.load(f)
+            obj_num = len(ann_data['objects'])
+            if obj_num >= total_obj_num_thre:
+                objects = ann_data['objects']
+                sample_info = {}
+                for obj in objects:
+                    label = obj['label']
+                    if label not in sample_info:
+                        sample_info[label] = [obj["bbox"]]
+                    else:
+                        sample_info[label].append(obj["bbox"])
+                file_id = os.path.splitext(ann_file_name)[0]
+                info[file_id] = sample_info
+
     return info
 
 
-def filter_samples():
+def filter_samples(ann_path, num_classes=2, second_thre=5):
     """
     Filter samples according to the number of samples per category.
-    :return:
     """
-    pass
+    with open(ann_path, 'r') as reader:
+        info = json.load(reader)
+    filtered_info = {}
+    for file_id, sample_info in info.items():
+        num_bboxes_list = [len(item) for item in sample_info.values()]
+        num_bboxes_list_sorted = sorted(num_bboxes_list, reverse=True)
+        if len(num_bboxes_list_sorted) >= num_classes and num_bboxes_list_sorted[1] >= second_thre:
+            filtered_info[file_id] = sample_info
+            print(file_id)
+    print(f"{len(filtered_info)} samples were kept after filtering.")
+    return filtered_info
+
+
+def task_parse_all_and_save_as_one():
+    """
+    Parse all the annotation files of the cityperson dataset, and save as one annotation file.
+    """
+    ann_dir = "D:\\Data\\BBH_Exp\\CityPersons\\gtBbox_cityPersons_trainval\\gtBboxCityPersons\\train"
+    tgt_file = r"D:\Data\BBH_Exp\CityPersons\multi_labels\annotation.json"
+    info = parse_cityperson_annotation(ann_dir=ann_dir)
+    with open(tgt_file, 'w') as writer:
+        json.dump(info, writer)
+
+
+def task_filter4multilabels():
+    in_json_file_path = r"D:\Data\BBH_Exp\CityPersons\multi_labels\annotation.json"
+    out_json_file_path = r"D:\Data\BBH_Exp\CityPersons\multi_labels\annotation_filtered.json"
+    num_classes = 2
+    second_thre = 10
+    filtered_info = filter_samples(in_json_file_path, num_classes=num_classes, second_thre=second_thre)
+    with open(out_json_file_path, 'w') as writer:
+        json.dump(filtered_info, writer)
 
 
 def main():
-    pass
+    #task_parse_all_and_save_as_one()
+    task_filter4multilabels()
 
 
 if __name__ == "__main__":
